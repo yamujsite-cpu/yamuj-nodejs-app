dde954330593368c50c00d1f57fa76be44551ba2
KareemGamal8 <kareemgmal7755@gmail.com>
2026-09-07 12:09:36 +0300

    Fix Service Model

--- modules/services/serviceModel.ts
+++ modules/services/serviceModel.ts
@@ -1,19 +1,24 @@
 import mongoose, { Schema } from "mongoose";
 
 const serviceItemSchema = new Schema({
-  subtitle: { type: String, default: "" },
-  title: { type: String, default: "" },
-  description: { type: String, default: "" },
+  subtitle: {
+    en: { type: String, default: "" },
+    ar: { type: String, default: "" },
+  },
+  title: {
+    en: { type: String, default: "" },
+    ar: { type: String, default: "" },
+  },
+  description: {
+    en: { type: String, default: "" },
+    ar: { type: String, default: "" },
+  },
   image: { type: String, default: "" },
 });
 
 const serviceSchema = new Schema(
   {
     sortOrder: { type: Number, default: 0 },
-    subtitle: {
-      en: { type: String, default: "" },
-      ar: { type: String, default: "" },
-    },
     title: {
       en: { type: String, default: "" },
       ar: { type: String, default: "" },
